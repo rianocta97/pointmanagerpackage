465fe76da5884538fa6eab3d10c0448fe6eac996
Rian Octa Fauzan <rian.octa@narindo.com>
2022-12-16 05:25:55 +0700

    testing, returning model

--- model/points.go
+++ model/points.go
@@ -1,9 +1,22 @@
 package model
 
-type GetBalanceParam struct {
+type StarGetBalanceRequestParam struct {
 	AccountNumber string `json:"accountNumber"`
 }
 
-type InquiryBalanceParam struct {
+type PartnerGetBalanceRequestParam struct {
 	AccountToken string `json:"accToken"`
+}
+
+type StarResponseData struct {
+	AccountNumber string  `json:"accountNumber"`
+	Balance       float64 `json:"balance"`
+	Name          string  `json:"name"`
+	StarData      bool    `json:"starData"`
+}
+
+type PartnerResponseData struct {
+	AccountNumber string  `json:"accountNumber"`
+	Balance       float64 `json:"balance"`
+	Name          string  `json:"name"`
 }
--- usecase/point.go
+++ usecase/point.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -13,31 +12,31 @@ import (
 )
 
 type PointUsecase interface {
-	CheckGetBalanceData(rawData interface{}) (*model.GetBalanceParam, *model.InquiryBalanceParam, error)
-	validateGetBalanceData(dataStar *model.GetBalanceParam, dataPartner *model.InquiryBalanceParam) error
-	PostData(dataStar *model.GetBalanceParam, dataPartner *model.InquiryBalanceParam) (map[string]interface{}, error)
+	CheckGetBalanceData(rawData interface{}) (*model.StarGetBalanceRequestParam, *model.PartnerGetBalanceRequestParam, error)
+	validateRequestData(dataStar *model.StarGetBalanceRequestParam, dataPartner *model.PartnerGetBalanceRequestParam) error
+	PostData(dataStar *model.StarGetBalanceRequestParam, dataPartner *model.PartnerGetBalanceRequestParam) (map[string]interface{}, error)
+	CheckGetBalanceResponse(rawData map[string]interface{}) (*model.StarResponseData, *model.PartnerResponseData, error)
+	validateResponseData(dataStar *model.StarResponseData, dataPartner *model.PartnerResponseData) error
 }
 
 type PointUsecaseImpl struct {
 }
 
-func (p *PointUsecaseImpl) CheckGetBalanceData(rawData interface{}) (*model.GetBalanceParam, *model.InquiryBalanceParam, error) {
+func (p *PointUsecaseImpl) CheckGetBalanceData(rawData interface{}) (*model.StarGetBalanceRequestParam, *model.PartnerGetBalanceRequestParam, error) {
 	data := rawData.(map[string]interface{})
 	if _, ok := data["accountNumber"]; ok {
-		dataStar := &model.GetBalanceParam{
+		dataStar := &model.StarGetBalanceRequestParam{
 			AccountNumber: data["accountNumber"].(string),
 		}
-		fmt.Println("datastar: ", dataStar)
-		if err := p.validateGetBalanceData(dataStar, nil); err != nil {
+		if err := p.validateRequestData(dataStar, nil); err != nil {
 			return nil, nil, err
 		}
 		return dataStar, nil, nil
 	} else if _, ok := data["accToken"]; ok {
-		dataPartner := &model.InquiryBalanceParam{
+		dataPartner := &model.PartnerGetBalanceRequestParam{
 			AccountToken: data["accToken"].(string),
 		}
-		fmt.Println("datapartner: ", dataPartner)
-		if err := p.validateGetBalanceData(nil, dataPartner); err != nil {
+		if err := p.validateRequestData(nil, dataPartner); err != nil {
 			return nil, nil, err
 		}
 		return nil, dataPartner, nil
@@ -46,7 +45,7 @@ func (p *PointUsecaseImpl) CheckGetBalanceData(rawData interface{}) (*model.GetB
 	return nil, nil, errors.New(constant.ErrorValidate_BalanceData)
 }
 
-func (p *PointUsecaseImpl) validateGetBalanceData(dataStar *model.GetBalanceParam, dataPartner *model.InquiryBalanceParam) error{
+func (p *PointUsecaseImpl) validateRequestData(dataStar *model.StarGetBalanceRequestParam, dataPartner *model.PartnerGetBalanceRequestParam) error{
 	// assumption: length will be 10 max
 	if dataStar != nil {
 		if (len(dataStar.AccountNumber) <= 0) || (len(dataStar.AccountNumber) > 10) {
@@ -62,12 +61,12 @@ func (p *PointUsecaseImpl) validateGetBalanceData(dataStar *model.GetBalancePara
 	return errors.New(constant.ErrorValidate_BalanceData)
 }
 
-func (p *PointUsecaseImpl) PostData(dataStar *model.GetBalanceParam, dataPartner *model.InquiryBalanceParam) (map[string]interface{}, error) {
+func (p *PointUsecaseImpl) PostData(dataStar *model.StarGetBalanceRequestParam, dataPartner *model.PartnerGetBalanceRequestParam) (map[string]interface{}, error) {
 	var postBody []byte
 	if dataStar != nil {
-		postBody, _ = json.Marshal(&dataStar)
+		postBody, _ = json.Marshal(dataStar)
 	} else if dataPartner != nil {
-		postBody, _ = json.Marshal(&dataPartner)
+		postBody, _ = json.Marshal(dataPartner)
 	} else {
 		return nil, errors.New(constant.ErrorValidate_BalanceData)
 	}
@@ -79,18 +78,58 @@ func (p *PointUsecaseImpl) PostData(dataStar *model.GetBalanceParam, dataPartner
 	}
 
 	defer resp.Body.Close()
-	fmt.Println("resp", resp)
 
-	body, err := ioutil.ReadAll(reqBody)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("body", body)
 
 	responses := make(map[string]interface{})
 	json.Unmarshal(body, &responses)
-	fmt.Println(responses)
 	return responses, nil
+}
+
+func (p *PointUsecaseImpl) CheckGetBalanceResponse(rawData map[string]interface{}) (*model.StarResponseData, *model.PartnerResponseData, error) {
+	if _, ok := rawData["starData"]; ok {
+		dataStar := &model.StarResponseData{
+			AccountNumber: rawData["accountNumber"].(string),
+			Balance: rawData["balance"].(float64),
+			Name: rawData["name"].(string),
+			StarData: rawData["starData"].(bool),
+		}
+		if err := p.validateResponseData(dataStar, nil); err != nil {
+			return nil, nil, err
+		}
+		return dataStar, nil, nil
+	} else {
+		// in this case, there's no difference between star & partner response data.
+		// so the "starData" param is added to differentiate between them
+		dataPartner := &model.PartnerResponseData{
+			AccountNumber: rawData["accountNumber"].(string),
+			Balance: rawData["balance"].(float64),
+			Name: rawData["name"].(string),
+		}
+		if err := p.validateResponseData(nil, dataPartner); err != nil {
+			return nil, nil, err
+		}
+		return nil, dataPartner, nil
+	}
+}
+
+func (p *PointUsecaseImpl) validateResponseData(dataStar *model.StarResponseData, dataPartner *model.PartnerResponseData) error{
+	// assumption: length will be 20 max
+	if dataStar != nil {
+		if (len(dataStar.AccountNumber) <= 0) || (len(dataStar.AccountNumber) > 20) {
+			return errors.New(constant.ErrorValidate_InvalidAccountNumber)
+		}
+		return nil
+	} else if dataPartner != nil {
+		if (len(dataPartner.AccountNumber) <= 0) || (len(dataPartner.AccountNumber) > 20) {
+			return errors.New(constant.ErrorValidate_InvalidAccountNumber)
+		}
+		return nil
+	}
+	return errors.New(constant.ErrorValidate_BalanceData)
 }
 
 func InitPointUsecase() PointUsecase {
